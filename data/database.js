608634c16fb2a0a412c57c07ac0ccf9876948ca6
unknown <badrehahaha@gmail.com>
2023-08-24 18:12:38 +0200

    ffppo

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ const { Client } = require("pg");
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Cela peut être nécessaire en fonction de la configuration de Railway
+    rejectUnauthorized: false 
   }
 });
 
